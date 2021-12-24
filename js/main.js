545eb4cdf6588760f0528d6c98a5ca3f49e28639
Maxim <Sen989@yandex.ru>
2021-12-24 18:02:49 +0700

    New24

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 "use strict"
 
-let number0fFilms = prompt("Сколько фильмов?");
+let number0fFilms;
+
+function start (){
+    number0fFilms = prompt("Сколько фильмов?");
+    while ( number0fFilms == "" || number0fFilms == null || isNaN(number0fFilms)){
+       number0fFilms = prompt("Сколько фильмов?"); 
+    }
+    
+}
+start();
 
 let personalMovieDB = {
     count : +number0fFilms,
@@ -9,7 +18,9 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 };
-for (let i = 0; i < 2; i++){
+
+function infoFilms () {
+    for (let i = 0; i < 2; i++){
     let nameFilms = prompt("Один из последних просмотренных фильмов?");
     let raitingFilms = prompt('На сколько оцените его?');
     if(nameFilms !=null &&  raitingFilms !=null && nameFilms != '' &&  raitingFilms !='' && nameFilms.length < 50){
@@ -18,13 +29,20 @@ for (let i = 0; i < 2; i++){
         i--;
     }
 };
-if(personalMovieDB.count < 10){
-    console.log("Просмотрено довольно мало фильмов")
-}else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
-    console.log("Вы классический зритель")
-}else if(personalMovieDB.count > 30){
-    console.log("Вы киноман")
-}else{
-    console.log("Произошла ошибка")
 }
+infoFilms ();
+
+function raitingUse(){
+    if(personalMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов")
+    }else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
+        console.log("Вы классический зритель")
+    }else if(personalMovieDB.count > 30){
+        console.log("Вы киноман")
+    }else{
+        console.log("Произошла ошибка")
+    }
+}
+raitingUse();
+
 console.log(personalMovieDB);
